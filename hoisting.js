f38923a702baadee8e08e2adc15499371e9ea62e
Stephen Wool <stevewool7@gmail.com>
2019-02-28 20:13:53 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -2,29 +2,29 @@ function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
+  var lyric = "maybe";
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  }
   sayMy();
 
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
